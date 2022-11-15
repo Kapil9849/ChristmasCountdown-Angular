81f20e7e85068825bfd5695f1d4e91205630cd95
Kapil9849 <Pushadapu.Kapil@emtecinc.com>
2022-11-15 07:20:00 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  endpoint: string = "https://localhost:44325/api/christmas";
   title = 'ChristmasCountdown';
+  response: any = "";
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+    this.http.get(this.endpoint).subscribe(respo => {
+      this.response = respo
+      this.response=this.response.summary;
+      console.log(this.response)
+    })
+
+  }
 }
